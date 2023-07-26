13d15c291cb862a4080c654a9ff5b34c33c33ebd
sucheta2010 <115158213+sucheta2010@users.noreply.github.com>
2023-07-26 17:56:22 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+noseX = 0;
+noseY = 0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    
+    canvas = createCanvas(550, 550);
+    canvas.position(560, 150);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log(poseNet is initialized);
+}
+
+ function gotPoses(results) {
+   if(results.length>0) {
+       console.log(results);
+   }  
+ }
+
+function draw() {
+    background('#969A97');
+}
